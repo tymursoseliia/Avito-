9a5856a98038e4f03e0d8c73c2b065223e672704
gorbacenkovitalij6-prog <tymursoseliia@gmail.com>
2026-05-01 10:04:21 +0300

    Fix mobile CSS showing hidden elements and update logo

--- replace_logo.py
+++ replace_logo.py
@@ -6,7 +6,7 @@ with open(html_file, 'r', encoding='utf-8') as f:
     html = f.read()
 
 # Replace the logo URL
-old_logo = 'https://70.img.avito.st/image/1/1.ogFkvLa5GOhSG4zuLN6wOkoeDO7WOYzu0hUu6tIfGOo.DD1WHCne7V9xcT0-BplTPVR-nMNUotlxjG7IidMeKtQ'
+old_logo = 'https://40.img.avito.st/image/1/1.k0QhGba102vP29z7NIf2E26_021P01z.p9tI3XW5mJ_B_8zFQQHkOOTa7M54qK2w2R2B6U99oKk'
 new_logo = './logo.jpg'
 
 html = html.replace(old_logo, new_logo)
